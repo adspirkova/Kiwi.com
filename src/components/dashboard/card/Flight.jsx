e10be1a8894753c5e5a59611323eabb44a673b5e
Tais-WD <taisiia.efremova@gmail.com>
2019-07-05 13:17:38 +0200

    my commit

--- src/components/dashboard/card/Flight.jsx
+++ src/components/dashboard/card/Flight.jsx
@@ -1,29 +1,81 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button } from 'reactstrap';
+import React from "react";
+import {
+  Card,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button
+} from "reactstrap";
 
-import { DateTime } from 'luxon';
+import { DateTime } from "luxon";
 
-const Flight = (props) => {
+const Flight = props => {
   return (
     <div>
-      <Card style={{
-        display: 'flex',
-        border: '1px solid black'
-      }}>
-        <CardBody>
-          <CardTitle>Flight number {props.index}</CardTitle>
-          <CardSubtitle>Price</CardSubtitle>
-          <CardText>{props.price} EUR </CardText>
-          <CardSubtitle>Time and Date</CardSubtitle>
+      <Card
+        style={{
+          margin: "2rem 10rem 2rem 21rem",
+          borderColor: "#333",
+          heigth: "50px",
+          width: "600px",
+          display: "flex",
+          alignItems: "center",
+          border: "1px solid black"
+        }}
+      >
+        <CardTitle
+          style={{
+            backgroundColor: "#9999ff",
+            fontSize: "30px",
+            border: "1px solid black",
+            padding: "1rem"
+          }}
+        >
+          Flight number {props.index}
+        </CardTitle>
 
-          <CardText>{DateTime.fromMillis(props.time * 1000).toFormat('hh:mm')}</CardText>
+        <CardBody
+          style={{
+            flexDirection: "row",
+            display: "flex",
+            justifyContent: "space-between",
+            width: "100%"
+          }}
+        >
+          <CardSubtitle>Price</CardSubtitle>
+          <CardSubtitle>Time and Date</CardSubtitle>
           <CardSubtitle>Duration</CardSubtitle>
-          <CardText>{props.duration}</CardText>
           <CardSubtitle>From - To</CardSubtitle>
-          <CardText>{props.from} - {props.to}</CardText>
-          <Button>Book!</Button>
         </CardBody>
+
+        <CardBody
+          style={{
+            flexDirection: "row",
+            display: "flex",
+            justifyContent: "space-between",
+            width: "100%"
+          }}
+        >
+          <CardText>{props.price} EUR </CardText>
+          <CardText>
+            {DateTime.fromMillis(props.time * 1000).toFormat("hh:mm")}
+          </CardText>
+          <CardText>{props.duration}</CardText>
+          <CardText>
+            {props.from} - {props.to}
+          </CardText>
+        </CardBody>
+
+        <Button
+          size="lg"
+          color="danger"
+          style={{
+            width: "50%"
+          }}
+        >
+          Book!
+        </Button>
       </Card>
     </div>
   );
